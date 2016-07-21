33196743c84345dd3506b7ffa4d0475742defa9b
Dennis Wallace <dwallace@shr.global>
2016-07-21 18:34:36 -0500

    more changes...4
    Signed-off-by: Dennis Wallace <dwallace@shr.global>

--- app/js/account.js
+++ app/js/account.js
@@ -493,10 +493,9 @@ function newCardItem(y,i) {
 	}
 	f.find('input[name="Contact__c"]').val( jQuery('#contactselect').val() );
 	
-	jQuery('<a></a>').attr('href','#').html('Delete').addClass('card-delete-button card-button-large').appendTo(f);
+	jQuery('<a></a>').attr('href','#').html('Delete').addClass('card-delete-button card-button-large').prependTo(f);
 	jQuery('<a></a>').attr('href','#').html('Save').addClass('form-save-button card-save-button card-button-large').appendTo(f);
-	jQuery('<a></a>').attr('href','#').html('Close').addClass('card-close-button').appendTo(f);
-	jQuery('<a></a>').attr('href','#').html('Edit').addClass('form-edit-link').appendTo(f);
+	jQuery('<a></a>').attr('href','#').html('Edit').addClass('form-edit-link card-edit-link').appendTo(f);
 	f.appendTo(c);
 	
 	return c;
