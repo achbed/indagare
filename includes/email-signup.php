3815e7f91e16df963b310231e624380e17dbcf9e
mac <mac@mbp17uni.local>
2016-08-04 10:55:50 -0500

    edit email signup to pass along first and last name when they exist

--- includes/email-signup.php
+++ includes/email-signup.php
@@ -2,8 +2,17 @@
 //fill in these values for with your own information
 $api_key = '2b2936e2f9a2f24744769910044b6b5b-us1';
 $datacenter = 'us1';
-$list_id = '4962a21673';
+//$list_id = '4962a21673'; // Indagare mailing list
+$list_id = '013366b0c9'; // Indagare Test mailing list
 $email = $_POST['email'];
+$fname = '';
+$lname = '';
+if( isset($_POST['fname']) ) {
+	$fname = $_POST['fname'];
+}
+if( isset($_POST['lname']) ) {
+	$lname = $_POST['lname'];
+}
 $status = 'subscribed';
 if(!empty($_POST['status'])){
     $status = $_POST['status'];
@@ -11,7 +20,7 @@ if(!empty($_POST['status'])){
 $url = 'https://'.$datacenter.'.api.mailchimp.com/3.0/lists/'.$list_id.'/members/';
 $username = 'apikey';
 $password = $api_key;
-$data = array("email_address" => $email,"status" => $status);
+$data = array("email_address" => $email,"status" => $status,"merge_fields" => array( 'FNAME' => $fname, 'LNAME' => $lname) );
 $data_string = json_encode($data);
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL,$url);
