aaa43861783b60586b46a8379e7c3e7a1b87717f
Alex Barber <abarber@shr.global>
2016-08-04 15:11:23 -0500

    revise CSS for footer form fields; edit placeholders for email, first, and last name

--- functions.php
+++ functions.php
@@ -6798,9 +6798,11 @@ function childtheme_override_siteinfo() {
         <p><?php print __('Receive our free email newsletter full of travel news, tips and advice.','indagare'); ?></p>
         <form class="newsletter-signup-form" method="POST" action="<?php echo get_bloginfo('stylesheet_directory'); ?>/includes/email-signup.php">
           <div class="form-combo">
-          	<span class="form-item"><input type="text" name="email" id="newsletter-signup" class="element newsletter-signup-input" placeholder="<?php print __('Email address (required)','indagare'); ?>" /></span>
-          	<span class="form-item"><input type="text" name="fname" class="element newsletter-signup-fname" placeholder="<?php print __('First name','indagare'); ?>" /></span>
-          	<span class="form-item"><input type="text" name="lname" class="element newsletter-signup-lname" placeholder="<?php print __('Last name','indagare'); ?>" /></span>
+          	<span class="form-item"><input type="text" name="email" id="newsletter-signup" class="element newsletter-signup-input" placeholder="<?php print __('Email address','indagare'); ?>" /></span>
+          </div>
+          <div class="form-combo">
+          	<span class="form-item"><input type="text" name="fname" class="element newsletter-signup-fname" placeholder="<?php print __('First name (optional)','indagare'); ?>" /></span>
+          	<span class="form-item"><input type="text" name="lname" class="element newsletter-signup-lname" placeholder="<?php print __('Last name (optionall)','indagare'); ?>" /></span>
           </div>
           <div class="buttons">
           	<button type="submit" class="primary button"><?php print __('Submit','indagare'); ?></button>
